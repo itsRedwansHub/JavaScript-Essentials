388852d2e4f79566654fead2771659890d1825b8
itsRedwansHub <itsredwan69@gmail.com>
2022-09-14 14:28:16 +0000

    Set up Sttucturea of game function

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calcAnswer(){
+
+}
+function incrementScore(){
+    
+}
+function incrementWrongAnswer(){
+
+}
+function displayAddition(){
+
+}
+function displaySubtract(){
+
+}
+function displayMultiply(){
+
+}
+function displayDivide(){
+
+}
